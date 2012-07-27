feaf3238bc1bf533e2f61ddf76a0677bcc0f2dc3
Mark Betnel <itmeson@gmail.com>
2012-07-27 15:11:05 -0400

    reorganized everything to work as a pelican blog.

---
+++ pelican.conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Mark Betnel"
+SITENAME = u"General Physics I"
+SITEURL = 'http://markbetnel.com/genphys'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('dy/dan', 'http://blog.mrmeyer.com/'),
+    ('Math Mistakes', 'http://mathmistakes.org/'),
+    ('Khan Academy', 'http://www.khanacademy.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+
+    
